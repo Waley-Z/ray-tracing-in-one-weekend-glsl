83dc77d25527994edd1200ba4811be15c272f168
Waley-Z <waleyz@umich.edu>
2024-02-21 15:45:09 -0500

    Task3 complete (again)

--- task3-ch6.glsl
+++ task3-ch6.glsl
@@ -5,25 +5,84 @@ struct Ray {
 
 ////////////////////////////////////////////// 
 //                  TASK 3                  //
-float hit_sphere(vec3 center, float radius, Ray r) {
-    vec3 oc = r.origin - center;
-    float a = dot(r.direction, r.direction);
-    float b = 2.0 * dot(oc, r.direction);
-    float c = dot(oc, oc) - radius * radius;
-    float discriminant = b * b - 4.0 * a * c;
+struct Interval {
+    float min;
+    float max;
+};
 
+bool interval_contains(Interval i, float x) {
+    return i.min <= x && x <= i.max;
+}
+
+bool interval_surrounds(Interval i, float x) {
+    return i.min < x && x < i.max;
+}
+
+struct Sphere {
+    vec3 center;
+    float radius;
+};
+
+struct HitRecord {
+    vec3 p;
+    vec3 normal;
+    float t;
+    bool front_face;
+};
+
+const int MAX_SPHERES = 10;
+Sphere spheres[MAX_SPHERES];
+int num_spheres = 0;
+
+bool hit_sphere(Sphere s, Ray r, Interval ray_t, out HitRecord rec) {
+    vec3 oc = r.origin - s.center;
+    float a = dot(r.direction, r.direction);
+    float half_b = dot(oc, r.direction);
+    float c = dot(oc, oc) - s.radius * s.radius;
+
+    float discriminant = half_b * half_b - a * c;
     if (discriminant < 0.0) {
-        return -1.0;
-    } else {
-        return (-b - sqrt(discriminant)) / (2.0 * a);
+        return false;
     }
+    float sqrtd = sqrt(discriminant);
+
+    float root = (-half_b - sqrtd) / a;
+    if (!interval_surrounds(ray_t, root)) {
+        root = (-half_b + sqrtd) / a;
+        if (!interval_surrounds(ray_t, root)) {
+            return false;
+        }
+    }
+
+    rec.t = root;
+    rec.p = r.origin + rec.t * r.direction;
+    vec3 outward_normal = (rec.p - s.center) / s.radius;
+    rec.front_face = dot(r.direction, outward_normal) < 0.0;
+    rec.normal = rec.front_face ? outward_normal : -outward_normal;
+
+    return true;
+}
+
+bool hit_world(Ray r, Interval ray_t, out HitRecord rec) {
+    HitRecord temp_rec;
+    bool hit_anything = false;
+    float closest_so_far = ray_t.max;
+
+    for (int i = 0; i < num_spheres; i++) {
+        if (hit_sphere(spheres[i], r, Interval(ray_t.min, closest_so_far), temp_rec)) {
+            hit_anything = true;
+            closest_so_far = temp_rec.t;
+            rec = temp_rec;
+        }
+    }
+
+    return hit_anything;
 }
 
 vec3 ray_color(Ray r) {
-    float t = hit_sphere(vec3(0.0, 0.0, -1.0), 0.5, r);
-    if (t > 0.0) {
-        vec3 N = normalize(r.origin + t * r.direction - vec3(0.0, 0.0, -1.0));
-        return 0.5 * vec3(N.x + 1.0, N.y + 1.0, N.z + 1.0);
+    HitRecord rec;
+    if (hit_world(r, Interval(0.0, 1.0/0.0), rec)) {
+        return 0.5 * (rec.normal + vec3(1.0, 1.0, 1.0));
     }
 //                                          //
 //////////////////////////////////////////////
@@ -39,6 +98,14 @@ void main() {
     float image_height = iResolution.y;
     float aspect_ratio = image_width / image_height;
 
+////////////////////////////////////////////// 
+//                  TASK 3                  //
+    // World
+    spheres[0] = Sphere(vec3(0.0, 0.0, -1.0), 0.5);
+    spheres[1] = Sphere(vec3(0.0, -100.5, -1.0), 100.0);
+    num_spheres = 2;
+//                                          //
+//////////////////////////////////////////////
     // Camera
     float focal_length = 1.0;
     float viewport_height = 2.0;
